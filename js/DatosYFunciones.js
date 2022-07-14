b423660db102d5e28faae1e49657183f9e171cb1
Gus <araeldecimoquintoangel@gmail.com>
2022-07-13 22:56:24 -0300

    Retoques menores

--- js/DatosYFunciones.js
+++ js/DatosYFunciones.js
@@ -103,6 +103,47 @@ const ingresarProductos = () => {
     console.log(listaProductos)
 }
 
+
+const opciones = () => {
+    rta  = prompt(Textos.bienvenida+`
+    1- Ingresar un producto nuevo
+    2- Actualizar stock
+    3- Simular carrito
+    4- Visualizar producto
+    5- Modificar producto`)
+    if (!validarNroConLimites(rta, 1, 5)){
+        alert(Textos.error);
+        rta=opciones();
+    }
+    return rta;
+}
+
+
+
+
+
+
+const seleccionSistema= (rta) => {
+    switch (parseInt(rta)) {
+        case 1:
+            ingresarProductos();
+            break;
+        case 2:
+            modificarStock();
+            break;
+        case 3:
+            agregarCarrito();
+            break;
+        case 4:
+            buscarProducto();
+            break;
+        default:
+            modificarProducto();
+            break;
+    }
+}
+
+
 const buscarProducto = () =>{
     const producto = ingresarString(Textos.nombre + " a buscar.", Textos.error);
     console.log(producto)
